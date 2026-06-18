7fe6c4f7984fbd29ee14d0045a6c34ca22034bd1
HoaBangChu <hoahuy20@gmail.com>
2026-06-19 00:30:19 +0700

    Complete cart screen

--- src/main/java/vn/edu/fpt/controller/customer/UpdateCart.java
+++ src/main/java/vn/edu/fpt/controller/customer/UpdateCart.java
@@ -8,10 +8,14 @@ import jakarta.servlet.http.HttpServletResponse;
 import jakarta.servlet.http.HttpSession;
 import vn.edu.fpt.dao.CartDAO;
 import vn.edu.fpt.dao.ProductDAO;
+import vn.edu.fpt.dto.response.CartResponse;
 import vn.edu.fpt.model.User;
 import vn.edu.fpt.util.ParamUtil;
 
 import java.io.IOException;
+import java.math.BigDecimal;
+import java.math.BigInteger;
+import java.util.List;
 
 /**
  * HoaNK - HE195013
@@ -31,35 +35,59 @@ public class UpdateCart extends HttpServlet {
             Integer cartItemId = ParamUtil.getInteger(request, "cart_item_id");
             Integer quantityItem = ParamUtil.getInteger(request, "quantity_item");
             Integer variantId = ParamUtil.getInteger(request, "variant_id");
-            // nếu quantity < 1
-         if(quantityItem < 1) {
-             response.getWriter().write("INVALID_STOCK");
+            Integer shopId = ParamUtil.getInteger(request, "shop_id");
+            // kiểm tra người dùng đăng nhập
+         HttpSession session = request.getSession();
+         User user = (User)session.getAttribute("user");
+         if(user == null) { // bắn về js để điều phối đến login
+             response.getWriter().write("{\"status\":\"UNAUTHORIZED\"}");
              return;
          }
-            // lấy ra số sản phẩm trong kho của item đó tránh việc + giỏ hàng nó hơn
+            // nếu quantity < 1
+         if(quantityItem < 1) {
+             response.getWriter().write("{\"status\":\"INVALID_STOCK\"}");
+             return;
+         }
+         // lấy ra số sản phẩm trong kho của item đó tránh việc + giỏ hàng nó hơn
              int currentStock = productDAO.getVariantStock(variantId);
          // nếu quantity + mà vượt mức cái trong kho thì bắn lỗi về
            if(quantityItem > currentStock) {
-               response.getWriter().write("OVER_STOCK");
+               response.getWriter().write("{\"status\":\"OVER_STOCK\"}");
                return;
            }
            // nếu không vượt quá thì cho vào update
-        boolean checkUpdate = updateQuantity(request, cartItemId, quantityItem);
+        boolean checkUpdate = cartDAO.updateQuantityItem(quantityItem, cartItemId, user.getUserId());
          // trả về cho js để load lại trang
            if(!checkUpdate) {
-               response.getWriter().write("ERROR");
-           }else{
-               response.getWriter().write("SUCCESS");
-           }
-         }
+               response.getWriter().write("{\"status\":\"ERROR\"}");
+           }else {
+               // nếu update thành công thì lấy ra list sản phẩm trong giỏ hàng của memeber đó
+               List<CartResponse> cartResponses = cartDAO.getCartForMember(user.getUserId());
 
-        // cập nhật số lượng
-    private boolean updateQuantity(HttpServletRequest request,int cartItemId, int quantity) {
-        HttpSession session = request.getSession();
-        User user = (User)session.getAttribute("user");
-        if(user != null) {
-            return cartDAO.updateQuantityItem(quantity, cartItemId, user.getUserId());
-        }
-        return false;
-    }
+               BigDecimal newShopTotal = new BigDecimal(BigInteger.ZERO); // tiền tất cả sản phẩm biến thể trog giỏ của shop đó
+               BigDecimal newAllShopTotal = new BigDecimal(BigInteger.ZERO); // tiền hcung trong giỏ tất cả sản phẩm có trong giỏ
+
+               // duyệt qua từng cart items để cộng tiền lại => tiền chung
+               if (cartResponses != null) {
+                   for (CartResponse c : cartResponses) {
+                       newAllShopTotal = newAllShopTotal.add(c.getTotalPrice());
+
+                       if (c.getShopId() == shopId) {
+                           newShopTotal = newShopTotal.add(c.getTotalPrice());
+                       }
+                   }
+               }
+
+               String shopTotalStr = "Tạm tính đơn hàng: " + String.format("%,d",newShopTotal.longValue()) + " đ";
+               String shopAllTotalStr = String.format("%,d", newAllShopTotal.longValue()) + " đ";
+                   // Trả về cho js
+                   String jsonResponse = "{"
+                           + "\"status\":\"SUCCESS\","
+                           + "\"newShopTotal\":\"" + shopTotalStr + "\","
+                           + "\"newAllShopTotal\":\"" + shopAllTotalStr + "\""
+                           + "}";
+
+                   response.getWriter().write(jsonResponse);
+               }
+     }
 }
